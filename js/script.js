11d3fb3068a98c8eff2dfe66428ea6640ff71b86
Kieran Devlin <kjrdevlin@gmail.com>
2020-01-08 10:30:11 -0800

    js overhaul - change loop format to callback function for quiz questions, loop works but working on bug in appending  answers loop, add mobile mixins to styles with variable, update styles

--- js/script.js
+++ js/script.js
@@ -1,23 +1,92 @@
 const quizOne = document.querySelector('.quiz-one');
 const quizTwo = document.querySelector('.quiz-two');
+const title = document.querySelector('.quiz-title');
+const quizQuestions = document.querySelector('.quiz-questions');
+const scoreContainer = document.querySelector('.score');
+let scoreCounter = document.querySelector('.user-score');
+let quiz = 0;
+let score = 0;
 
 const json = quiz =>
   fetch('./src/quiz.json')
     .then(function(resp) {
+      //returns json response
       return resp.json();
     })
     .then(function(data) {
-      console.log(data.quizzes[quiz]);
+      //can now access json file locally
+      let currentQuizData = data.quizzes[quiz];
+      appendQuestions(currentQuizData, quiz);
     });
 
-const quizOneInitiate = () => {
-  console.log('quiz one start');
-  json(0);
+const appendQuestions = (q, quiz, iterator = 0) => {
+  //removes quiz selector
+  quizQuestions.innerHTML = '';
+  let ar = q.questions;
+  let answers = ar[quiz].answers;
+  // let i = 0;
+
+  // const jsonLoop = (ar, callback) => {
+
+  //   loop();
+
+  //   function loop() {
+  if (iterator < ar.length) {
+    // appends question in json file
+    title.innerHTML = ar[iterator].question;
+
+    // appends possible answers to current question in json file
+    for (let i = 0; i < answers.length; i++) {
+      const b = document.createElement('button');
+      const a = document.createElement('h3');
+      quizQuestions.appendChild(b);
+      b.appendChild(a);
+      a.innerHTML = answers[i].content;
+      console.log(answers[i].content);
+      b.classList.add('number:' + i);
+    }
+
+    //selects all the buttons and adds event listener to all
+    document.querySelectorAll('button').forEach(el => {
+      el.addEventListener('click', () => {
+        let submitAnswer = el.classList.value.slice(
+          el.classList.value.length - 1,
+          el.classList.value.length
+        );
+
+        // checks if current answer is the correct answer
+        if (answers[submitAnswer].value === true) {
+          score++;
+          iterator++;
+          scoreCounter.innerHTML = score;
+          appendQuestions(q, quiz, iterator);
+        }
+      });
+    });
+  } else {
+    console.log('broken');
+    // callback();
+  }
+  //   }
+  // };
+
+  // jsonLoop(ar);
 };
 
-const quizTwoInitiate = () => {
-  console.log('quiz two start');
-  json(1);
+// grabs only quiz 1 of JSON file
+const quizOneInitiate = quiz => {
+  quiz = 0;
+  scoreContainer.classList.add('active');
+  scoreCounter.innerHTML = score;
+  json(quiz);
+};
+
+// grabs only quiz 2 of JSON file
+const quizTwoInitiate = quiz => {
+  quiz = 1;
+  scoreContainer.classList.toggle('active');
+  scoreCounter.innerHTML = score;
+  json(quiz);
 };
 
 quizOne.addEventListener('click', e => {
@@ -29,3 +98,19 @@ quizTwo.addEventListener('click', e => {
   e.preventDefault();
   quizTwoInitiate();
 });
+
+////////////////////////////////////////////////////////////////////
+// THIS LOOP IS INFINITE DONT USE
+////////////////////////////////////////////////////////////////////
+
+//loops through questions and answers and appends to DOM
+// for (let i = 0; i < currentQuiz.questions.length; i++) {
+//   title.innerHTML = currentQuiz.questions[i].question;
+//   currentQuiz.questions[quiz].answers.forEach(e => {
+//     const b = document.createElement('button');
+//     const a = document.createElement('h3');
+//     quizQuestions.appendChild(b);
+//     b.appendChild(a);
+//     a.innerHTML = e.content;
+//   });
+// }
